291d45d16aa80f73d51298150e004bbda20e754f
Aleti Srinivasarao <alti@Administrators-MacBook-Pro-2.local>
2019-11-21 16:40:07 +0530

    Add appollo client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    </ApolloProvider>
   );
 }
 
