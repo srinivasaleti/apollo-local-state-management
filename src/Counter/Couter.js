48026a12828b7e8925a1ea37095c4eb10a6a8dd0
Aleti Srinivasarao <alti@Administrators-MacBook-Pro-2.local>
2019-11-21 18:13:49 +0530

    Implement Simple Counter

--- src/Counter/Couter.js
+++ src/Counter/Couter.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import styles from './styles.css'
 
-const GET_COUNTER = gql`
+export const GET_COUNTER = gql`
   query GetCounterValue {
     counter @client
   }
 `;
 
+const UPDATE_COUNTER = gql`
+  mutation updateCounter($offset: Number!) {
+    updateCounter(offset: $offset) @client
+  }
+`;
+
 export default function Couter() {
   const { data } = useQuery(GET_COUNTER);
+  const [increment] = useMutation(UPDATE_COUNTER, { variables: { offset: 1 } })
+  const [decrement] = useMutation(UPDATE_COUNTER, { variables: { offset: -1 } })
+
   return (
     <div>
       <h1>Couter: {data.counter}</h1>
       <div className="controllers">
-        <button>Add</button>
-        <button>Remove</button>
+        <button onClick={increment}>Add</button>
+        <button onClick={decrement}>Remove</button>
       </div>
     </div>
   )
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag';
+import { GET_COUNTER } from './Counter/Couter';
 
 export const typeDefs = gql`
   extend type Query {
@@ -6,4 +7,12 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {};
+export const resolvers = {
+    Mutation: {
+        updateCounter: (_, variables, { cache }) => {
+            const data = cache.readQuery({ query: GET_COUNTER });
+            cache.writeData({ data: { counter: data.counter + variables.offset } });
+            return null
+        },
+    },
+};
